1d4198e65daca3a7bfc227d76858b0e74af47823
bactaholic <mail@buckles.email>
2025-05-05 18:28:46 -0700

    optimization

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:convert';
 import '../models/settings.dart' as app_settings;
 import '../models/book.dart';
